73d025d128e66ffe4cb79e717af65d2cac615cb1
Masud Ibn Musa <masudibnmusa10@gmail.com>
2026-04-30 00:01:41 +0600

    Add multiply and  division function with zero error handling

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 print("Name : Masud Ibn Musa")
 print("Date : 29-04-2026")
 
-from utils import add, subtract  
+from utils import add, subtract, divide , multiply
 
 result_add = add(10, 5)
 result_sub = subtract(10, 5)
 
 print(f"Addition of 10 + 5 = {result_add}")
 print(f"Subtraction of 10 - 5 = {result_sub}")
+
+print("Division (10 * 2):", multiply(10, 2))
+
+print("Division (10 / 2):", divide(10, 2))
+print("Division (10 / 0):", divide(10, 0))
