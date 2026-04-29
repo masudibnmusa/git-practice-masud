99b0f8e08b87dc5cadcba2251cae2c293e945b6c
Masud Ibn Musa <masudibnmusa10@gmail.com>
2026-04-29 23:44:43 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
 print("Name : Masud Ibn Musa")
 print("Date : 29-04-2026")
+
+from utils import add, subtract  
+
+result_add = add(10, 5)
+result_sub = subtract(10, 5)
+
+print(f"Addition of 10 + 5 = {result_add}")
+print(f"Subtraction of 10 - 5 = {result_sub}")
